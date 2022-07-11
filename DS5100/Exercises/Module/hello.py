7756284626ca6f5b2ec85c2bbc49def867f9e8bb
kylerhalat-shafer <kyler.halatshafer@gmail.com>
2022-07-11 11:29:34 -0400

    Add files via upload
    Module

---
+++ DS5100/Exercises/Module/hello.py
@@ -0,0 +1,63 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "5d195fe5-1240-4c42-8a9a-2ae3ec163fe7",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def hello():\n",
+    "    '''This will print, Hello World'''\n",
+    "    print('Hello World')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "b1c55d73-d7fe-427c-99e2-2c852dbd5475",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello World\n"
+     ]
+    }
+   ],
+   "source": [
+    "hello()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "400119b2-3f64-45a2-af45-f650f60a4a58",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
